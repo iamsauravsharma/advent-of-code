9980fde5336c5d62e43ac08a065b4c40207692b1
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-07-23 11:32:43 +0545

    Update poetry lock and run repeated code by calling function
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- advent_of_code/cache_file.py
+++ advent_of_code/cache_file.py
@@ -1,6 +1,5 @@
 import appdirs
 import os
-import shutil
 from pathlib import Path
 from .server_action import download_input
 
@@ -9,10 +8,7 @@ def check_if_downloaded(year, day, session):
     """
     Check if really a input is downloaded or not and cached
     """
-    cache_location = appdirs.user_cache_dir()
-    cache_file = os.path.join(
-        cache_location, str(session), str(year), str(day), "input.txt"
-    )
+    cache_file = join_path(year, day, session, input=True)
     cache_file = Path(cache_file)
     return cache_file.exists()
 
@@ -21,10 +17,7 @@ def save_input_to_location(year, day, session, input):
     """
     Save a input to its location
     """
-    cache_location = appdirs.user_cache_dir()
-    cache_folder = os.path.join(
-        cache_location, "advent_of_code", str(session), str(year), str(day)
-    )
+    cache_folder = join_path(year, day, session)
     Path(cache_folder).mkdir(parents=True, exist_ok=True)
     cache_file = os.path.join(cache_folder, "input.txt")
     with open(cache_file, "w+") as f:
@@ -35,12 +28,9 @@ def delete_input(year, day, session):
     """
     Delete input from a cache folders
     """
-    cache_location = appdirs.user_cache_dir()
-    cache_folder = os.path.join(
-        cache_location, "advent_of_code", str(session), str(year), str(day)
-    )
-    if Path(cache_folder).exists():
-        shutil.rmtree(cache_folder)
+    cache_file = join_path(year, day, session, input=True)
+    if Path(cache_file).exists():
+        os.remove(cache_file)
 
 
 def cache_file_data(year, day, session):
@@ -48,10 +38,15 @@ def cache_file_data(year, day, session):
     Return cache file data from cache
     """
     download_input(year, day, session)
-    cache_location = appdirs.user_cache_dir()
-    cache_file = os.path.join(
-        cache_location, str(session), str(year), str(day), "input.txt"
-    )
+    cache_file = join_path(year, day, session, input=True)
     with open(cache_file) as f:
         input_data = f.read()
     return input_data
+
+
+def join_path(year, day, session, input=False):
+    cache_location = appdirs.user_cache_dir()
+    cache_file = os.path.join(cache_location, str(session), str(year), str(day))
+    if input:
+        cache_file = os.path.join(cache_file, "input.txt")
+    return cache_file
