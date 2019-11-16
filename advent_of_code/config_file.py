35b803cafeb43d17b4638fc6b6759fe820a6e3f2
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-11-16 11:49:04 +0545

    chore: add some typehint for function
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- advent_of_code/config_file.py
+++ advent_of_code/config_file.py
@@ -2,18 +2,18 @@
 import json
 import os
 from pathlib import Path
-
+from typing import List
 import appdirs
 
 
 class Data:
     """Class to store data about a config file of advent-of-code"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: str):
         """Initialize Data class"""
         self.session_list = kwargs
 
-    def to_json(self):
+    def to_json(self) -> str:
         """Convert Data to JSON type"""
         return json.dumps(self.__dict__, indent=4, sort_keys=True)
 
@@ -24,11 +24,11 @@ class Data:
         json_data = json_dict.get("session_list")
         return cls(**json_data)
 
-    def add_session(self, **kwargs):
+    def add_session(self, **kwargs: str):
         """Add new session or update session in a config file Data type."""
         self.session_list.update(kwargs)
 
-    def delete_session(self, session):
+    def delete_session(self, session: str):
         """Delete Session from config files"""
         try:
             self.session_list.pop(session)
@@ -36,7 +36,7 @@ class Data:
             raise Exception("Value with name {} not found over config".format(session))
 
 
-def _read_json_file(config_file):
+def _read_json_file(config_file: str) -> Data:
     """Read JSON file to return Data type"""
     if not config_file.exists():
         with open(config_file, "a+") as opened_file:
@@ -48,7 +48,7 @@ def _read_json_file(config_file):
     return data
 
 
-def _config_file_data():
+def _config_file_data() -> str:
     """Get Config file location"""
     config_location = appdirs.user_config_dir()
     config_file = os.path.join(config_location, "aoc-config.json")
@@ -56,7 +56,7 @@ def _config_file_data():
     return config_file
 
 
-def add_to_json(**kwargs):
+def add_to_json(**kwargs: str):
     """Add new session to json config file"""
     config_file = _config_file_data()
     data = _read_json_file(config_file)
@@ -65,7 +65,7 @@ def add_to_json(**kwargs):
         opened_file.write(data.to_json())
 
 
-def delete_from_json(session):
+def delete_from_json(session: str):
     """Delete session from JSON config file"""
     config_file = _config_file_data()
     data = _read_json_file(config_file)
@@ -84,7 +84,7 @@ def list_from_json():
         print("{} = {}".format(key, val))
 
 
-def get_session_value(session_name):
+def get_session_value(session_name: str) -> str:
     """Return session name value from JSON config file"""
     config_file = _config_file_data()
     data = _read_json_file(config_file)
@@ -96,7 +96,7 @@ def get_session_value(session_name):
     raise Exception("{} key is not present in config file".format(session_name))
 
 
-def get_all_session():
+def get_all_session() -> List[str]:
     """Return all session name"""
     config_file = _config_file_data()
     data = _read_json_file(config_file)
