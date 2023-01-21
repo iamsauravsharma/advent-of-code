4786ff92d318de6ec994ea4ae6f07fc76ea40dd2
Saurav Sharma <saurav.sharma@nepware.com>
2023-01-21 12:35:51 +0545

    ci: add test related to python version 3.11.1
    Signed-off-by: Saurav Sharma <saurav.sharma@nepware.com>

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,12 @@
 import nox_poetry
 from nox_poetry.sessions import Session
 
-python_versions = ["3.7", "3.8", "3.9", "3.10"]
+python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
 
 @nox_poetry.session(python=python_versions)
 def lint(session: Session):
-    session.install("flake8", "black", "isort", "safety", "pytest", ".")
-    session.run("flake8")
+    session.install("black", "isort", "safety", "pytest", ".")
     session.run("black", ".", "--check")
     session.run("isort", ".", "--check-only")
     requirements_file = session.poetry.export_requirements()
