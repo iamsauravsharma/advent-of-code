65ddc97f16d7407647c9e1a9929ed43da0629f5f
Saurav Sharma <appdroiddeveloper@gmail.com>
2024-10-17 20:26:35 +0545

    test: do not install safety in nox session
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ python_versions = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
 @session(python=python_versions)
 def lint(session: Session):
-    session.install("ruff", "safety", "pytest", ".")
+    session.install("ruff", "pytest", ".")
     session.run("ruff", "check", ".")
     session.run("ruff", "format", ".", "--check")
     session.run("pytest")
